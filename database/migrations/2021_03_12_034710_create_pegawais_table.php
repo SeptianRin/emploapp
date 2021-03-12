e6097c1ea29007651061e3398c399d8440a2d413
septianrin <septianrinaldi98@gmail.com>
2021-03-12 13:21:47 +0700

    new project

---
+++ database/migrations/2021_03_12_034710_create_pegawais_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->string('empid');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('email');
+            $table->date('tanggal_masuk');
+            $table->bigInteger('gaji');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawais');
+    }
+}
